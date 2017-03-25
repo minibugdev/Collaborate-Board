f7f22492970472486d874914582d9b2b94f306b0
minibugdev <minibugdev@gmail.com>
2017-03-25 16:29:28 +0700

    remove drawview library

--- app/src/main/kotlin/com/trydroid/coboard/CoBoardActivity.kt
+++ app/src/main/kotlin/com/trydroid/coboard/CoBoardActivity.kt
@@ -1,15 +1,10 @@
 package com.trydroid.coboard
 
+import android.graphics.Point
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
-import com.byox.drawview.dictionaries.DrawMove
-import com.byox.drawview.enums.DrawingMode
 import com.google.firebase.database.*
-import com.trydroid.coboard.views.ExtenedDrawView
-import kotlinx.android.synthetic.main.activity_main.*
 
 class CoBoardActivity : AppCompatActivity() {
 	private val mLinesReference: DatabaseReference by lazy { FirebaseDatabase.getInstance().reference.child(CHILD_LINES) }
@@ -20,48 +15,21 @@ class CoBoardActivity : AppCompatActivity() {
 		setupDrawView()
 	}
 
-	override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-		menuInflater.inflate(R.menu.menu_co_board, menu)
-		return super.onCreateOptionsMenu(menu)
-	}
-
 	override fun onStart() {
 		super.onStart()
 		mLinesReference.addChildEventListener(mLinesReferenceListener)
-		drawView.setOnDrawViewListener(mDrawViewListener)
 
 	}
 
 	override fun onStop() {
 		super.onStop()
 		mLinesReference.removeEventListener(mLinesReferenceListener)
-		drawView.setOnDrawViewListener(null)
 	}
-
-	override fun onOptionsItemSelected(item: MenuItem?) =
-		when (item?.itemId) {
-			R.id.action_clear -> consumeMenuSelected { clearDrawView() }
-			R.id.action_undo  -> consumeMenuSelected { undoDrawView() }
-			R.id.action_redo  -> consumeMenuSelected { redoDrawView() }
-			else              -> super.onOptionsItemSelected(item)
-		}
 
 	private fun setupDrawView() {
 	}
 
-	private fun redoDrawView() {
-		if (drawView.canRedo()) drawView.redo()
-	}
-
-	private fun undoDrawView() {
-		if (drawView.canUndo()) drawView.undo()
-	}
-
-	private fun clearDrawView() {
-		drawView.restartDrawing()
-	}
-
-	private fun sendToFirebase(drawMove: DrawMove) {
+	private fun sendToFirebase(drawMove: Point) {
 		mLinesReference.push().setValue(drawMove)
 	}
 
@@ -82,27 +50,6 @@ class CoBoardActivity : AppCompatActivity() {
 		override fun onCancelled(databaseError: DatabaseError) {
 			Log.w(TAG, "Lines Reference onCancelled", databaseError.toException())
 		}
-	}
-
-	private val mDrawViewListener = object : ExtenedDrawView.OnDrawViewListener {
-		override fun onStartDrawing() {
-		}
-
-		override fun onEndDrawing() {
-			val lastestDrawMove = drawView.drawMoveHistory.lastOrNull { drawMove -> DrawingMode.DRAW == drawMove.drawingMode }
-			lastestDrawMove?.let { sendToFirebase(it) }
-		}
-
-		override fun onRequestText() {
-		}
-
-		override fun onClearDrawing() {
-		}
-	}
-
-	inline fun consumeMenuSelected(func: () -> Unit): Boolean {
-		func()
-		return true
 	}
 
 	companion object {
